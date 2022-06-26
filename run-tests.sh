2e220650efadfdf287767774a469f4392466352b
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-06-26 23:24:05 +0200

    move check_manifest configuration to setup.cfg.
    concentrate the configuration of all calls in one place

--- run-tests.sh
+++ run-tests.sh
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2020 CERN.
+# Copyright (C) 2022 Graz University of Technology.
 #
 # Babel-EDTF is free software; you can redistribute it and/or modify it under
 # the terms of the MIT License; see LICENSE file for more details.
 
-python -m check_manifest --ignore ".*-requirements.txt" && \
+python -m check_manifest && \
 python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
 python -m pytest && \
 python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
