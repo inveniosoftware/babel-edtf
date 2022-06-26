841b84a07269bcb0e296ead07151efb5c284808f
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-06-26 23:26:49 +0200

    global: clean test infrastructure

--- run-tests.sh
+++ run-tests.sh
@@ -7,7 +7,12 @@
 # Babel-EDTF is free software; you can redistribute it and/or modify it under
 # the terms of the MIT License; see LICENSE file for more details.
 
-python -m check_manifest && \
-python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
-python -m pytest && \
-python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+python -m check_manifest
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+python -m pytest
