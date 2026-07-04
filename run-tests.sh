88c5be040f4bdf895490ae48218087b64553bdfd
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-04 22:03:18 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * format pyproject.toml with Tombi
    * replace pycodestyle with black
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,6 @@
 # SPDX-FileCopyrightText: 2020 CERN.
 # SPDX-License-Identifier: MIT
 
-python -m check_manifest --ignore ".*-requirements.txt" && \
 python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
 python -m pytest && \
 python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
